8a8e176c2927e1573456a92c1ba3d219d3b36cc3
qmf123456 <37503341+qmf123456@users.noreply.github.com>
2018-03-18 20:49:41 +0800

    Add files via upload

---
+++ jiaoben.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    $("#logo").mouseover(function () {
+        $("#logo").toggleClass("animated wobble");
+    });
+})
+function time(){
+    var aa=new Date();
+    document.getElementById("time").innerHTML=aa.getFullYear();
+}
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https'){
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else{
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+
+
+$("#mytab a").click(function (e) {
+    $(this).tab('show');
+})
+$('.carousel').carousel({
+    interval: 4000
+})
+
